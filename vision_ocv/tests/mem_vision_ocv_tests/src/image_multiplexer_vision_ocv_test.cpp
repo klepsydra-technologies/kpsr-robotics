707a19ede3b26fc8f2e79796b43782d7fc67468c
Adithya Pokala <adithyapokala@outlook.com>
2022-04-19 20:09:27 +0200

    fix test and format

--- vision_ocv/tests/mem_vision_ocv_tests/src/image_multiplexer_vision_ocv_test.cpp
+++ vision_ocv/tests/mem_vision_ocv_tests/src/image_multiplexer_vision_ocv_test.cpp
@@ -58,6 +58,13 @@ TEST(HPVisionTest, HPVisionTest)
         slowReadService.execute();
     }
 
+    int numAttempts = 10;
+    for (int i = 0; i < numAttempts; i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        if (simpleReadService.receivedEvents == 50)
+            break;
+    }
+
     writeService.stop();
     simpleReadService.stop();
     slowReadService.stop();
